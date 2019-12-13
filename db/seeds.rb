65f60ed65b1f32928995a22ee9f9f6a3662f4a3b
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-13 15:51:53 +0000

    manufacturer class created and ran delete_all, all and find methods

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,20 @@ require_relative('../models/guitar.rb')
 require_relative('../models/manufacturer.rb')
 require('pry')
 
+Manufacturer.delete_all()
+
 manufacturer1 = Manufacturer.new({
   'name' => "Martin"
   })
+manufacturer1.save()
+manufacturer2 = Manufacturer.new({
+  'name' => "Gibson"
+  })
+manufacturer2.save()
+manufacturer3 = Manufacturer.new({
+  'name' => "Fender"
+  })
+manufacturer3.save()
 
 binding.pry
 nil
--- models/manufacturer.rb
+++ models/manufacturer.rb
@@ -10,4 +10,31 @@ class Manufacturer
     @name = options['name']
   end
 
+  def save()
+    sql = "INSERT INTO manufacturers (
+          name
+          ) VALUES ($1) RETURNING id;"
+    values = [@name]
+    @id = SqlRunner.run(sql, values)[0]['id'].to_i
+  end
+
+  def self.all()
+    sql = "SELECT * FROM manufacturers;"
+    results = SqlRunner.run(sql)
+    return results.map{|manufacturer| Manufacturer.new(manufacturer)}
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM manufacturers;"
+    SqlRunner.run(sql)
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM manufacturers
+          WHERE id = $1;"
+    values = [id]
+    result = SqlRunner.run(sql, values)
+    return Manufacturer.new(result.first)
+  end
+
 end
