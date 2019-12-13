c9b993459a99898d5bf7384c26ca19f7c4ec1447
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-13 14:43:17 +0000

    manufacturer class created and ran using seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+require_relative('../models/guitar.rb')
+require_relative('../models/manufacturer.rb')
+require('pry')
+
+manufacturer1 = Manufacturer.new({
+  'name' => "Martin"
+  })
+
+binding.pry
+nil
