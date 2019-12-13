ef1fe5ce104c2f3ff50354079ba26b05a4369e48
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-13 14:06:18 +0000

    sql written and run to create tables

---
+++ db/guitars.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS guitars;
+DROP TABLE IF EXISTS manufacturers;
+
+CREATE TABLE manufacturers
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+CREATE TABLE guitars
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  stock_quantity INT,
+  buying_cost INT,
+  selling_price INT,
+  manufacturer_id INT REFERENCES manufacturers(id)
+);
