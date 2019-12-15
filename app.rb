c1c0b826f9a996ba578fe54acd761330bb704432
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-15 15:15:50 +0000

    refactored to have multiple controller files

--- app.rb
+++ app.rb
@@ -1,15 +1,9 @@
 require('sinatra')
 require('sinatra/contrib/all')
-# require_relative('controllers/...')
-require_relative('./models/guitar')
+require_relative('controllers/guitar_controller')
 require_relative('./models/manufacturer')
 also_reload('./models/*')
 
 get '/' do
   erb(:index)
-end
-
-get '/guitars' do
-  @guitars = Guitar.all()
-  erb(:"guitars/index")
 end
---
+++ controllers/guitar_controller.rb
@@ -0,0 +1,14 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/guitar')
+also_reload('./models/*')
+
+get '/guitars' do
+  @guitars = Guitar.all()
+  erb(:"guitars/index")
+end
+
+get '/guitars/:id' do
+  @guitar = Guitar.find(params['id'].to_i)
+  erb(:"guitars/show")
+end
