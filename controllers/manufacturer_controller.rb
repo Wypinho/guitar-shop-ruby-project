b541eb0ac79c2a2f1144fde09c1cd2032a356ff5
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-15 18:14:43 +0000

    display all manufacturers added

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -7,12 +7,12 @@ get '/manufacturers' do
   @manufacturers = Manufacturer.all()
   erb(:"manufacturers/index")
 end
-#
-# get '/manufacturers/:id' do
-#   @guitar = Guitar.find(params['id'].to_i)
-#   erb(:"manufacturers/show")
-# end
-#
+
+get '/manufacturers/:id' do
+  @manufacturer = Manufacturer.find(params['id'].to_i)
+  erb(:"manufacturers/show")
+end
+
 # get '/manufacturers/:id/edit' do
 #   @guitar = Guitar.find(params['id'].to_i)
 #   @manufacturers = Manufacturer.all()
