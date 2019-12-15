d0a18243a4078fc8814698dae3b67d7f9e55214d
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-15 17:08:54 +0000

    guitar edit screen created

--- controllers/guitar_controller.rb
+++ controllers/guitar_controller.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/guitar')
+require_relative('../models/manufacturer')
 also_reload('./models/*')
 
 get '/guitars' do
@@ -13,8 +14,13 @@ get '/guitars/:id' do
   erb(:"guitars/show")
 end
 
-get '/guitar/:id/edit' do
-  guitar = Guitar.find(params['id'].to_i)
-  guitar.update()
+get '/guitars/:id/edit' do
+  @guitar = Guitar.find(params['id'].to_i)
+  @manufacturers = Manufacturer.all()
   erb(:"guitars/edit")
+end
+
+post '/guitars/:id' do
+  Guitar.new(params).update
+  redirect to '/guitars'
 end
