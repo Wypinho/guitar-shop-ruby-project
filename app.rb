3494d7fb587ba3fcf902200d588c4ce0c3b7551a
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-15 15:03:53 +0000

    created nav element and can now show all guitars in stock by name

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require('sinatra')
 require('sinatra/contrib/all')
 # require_relative('controllers/...')
+require_relative('./models/guitar')
+require_relative('./models/manufacturer')
 also_reload('./models/*')
 
 get '/' do
   erb(:index)
+end
+
+get '/guitars' do
+  @guitars = Guitar.all()
+  erb(:"guitars/index")
 end
