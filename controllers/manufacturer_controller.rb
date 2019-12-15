7a34f4773bf205e1119051ff2169cbe4a5fa650b
Andrew Wyper <andy_wyper@yahoo.co.uk>
2019-12-15 19:20:28 +0000

    can add new manufacturers

--- controllers/manufacturer_controller.rb
+++ controllers/manufacturer_controller.rb
@@ -8,11 +8,22 @@ get '/manufacturers' do
   erb(:"manufacturers/index")
 end
 
+get '/manufacturers/new' do
+  erb(:"manufacturers/new")
+end
+
+post '/manufacturers' do
+  manufacturer = Manufacturer.new(params)
+  manufacturer.save()
+  redirect to '/manufacturers'
+end
+
 get '/manufacturers/:id' do
   @manufacturer = Manufacturer.find(params['id'].to_i)
   @guitars = @manufacturer.guitars()
   erb(:"manufacturers/show")
 end
+
 
 # get '/manufacturers/:id/edit' do
 #   @guitar = Guitar.find(params['id'].to_i)
